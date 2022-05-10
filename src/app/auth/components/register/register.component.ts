94ee2e79f7717a2d54a663987842d89894c1e225
Matheus Artur <matheuscabral@protonmail.com>
2022-05-10 14:31:14 -0300

    add ngrx

--- src/app/auth/components/register/register.component.ts
+++ src/app/auth/components/register/register.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from "@angular/core";
 import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { Store } from "@ngrx/store";
+
+import { registerAction } from "src/app/auth/store/actions";
 
 @Component({
   selector: "d-register",
@@ -9,7 +12,7 @@ import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 export class RegisterComponent implements OnInit {
   form: FormGroup;
 
-  constructor(private fb: FormBuilder) {
+  constructor(private fb: FormBuilder, private store: Store) {
     this.form = this.fb.group({
       username: ["", Validators.required],
       email: ["", Validators.required],
@@ -20,6 +23,7 @@ export class RegisterComponent implements OnInit {
   ngOnInit(): void {}
 
   onSubmit(): void {
+    this.store.dispatch(registerAction(this.form.value));
     console.log("submit", this.form.value);
   }
 }

8f2cac8a309f5eec89f5627725c499975c34088a
Matheus Artur <matheuscabral@protonmail.com>
2022-05-10 13:07:25 -0300

    routing

---
+++ src/app/auth/components/register/register.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "d-register",
+  templateUrl: "./register.component.html",
+  styleUrls: ["./register.component.scss"],
+})
+export class RegisterComponent {}

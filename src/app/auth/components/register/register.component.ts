811d17b9f9f16658e9c8d49646973852531a248d
Matheus Artur <matheuscabral@protonmail.com>
2022-05-10 13:40:34 -0300

    Register page

--- src/app/auth/components/register/register.component.ts
+++ src/app/auth/components/register/register.component.ts
@@ -1,8 +1,25 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 
 @Component({
   selector: "d-register",
   templateUrl: "./register.component.html",
   styleUrls: ["./register.component.scss"],
 })
-export class RegisterComponent {}
+export class RegisterComponent implements OnInit {
+  form: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      username: ["", Validators.required],
+      email: ["", Validators.required],
+      password: ["", Validators.required],
+    });
+  }
+
+  ngOnInit(): void {}
+
+  onSubmit(): void {
+    console.log("submit", this.form.value);
+  }
+}
